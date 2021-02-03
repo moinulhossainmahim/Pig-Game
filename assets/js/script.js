dfd825e1b44403d45d51734217e81584c6c963e1
Moinul Hossain <moinulhossainmahim@gmail.com>
2021-02-03 22:11:59 +0600

    initial commit with html

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+/*
+GAME RULES:
+
+- The game has two players, playing in rounds
+- In each turn, a player rolls a dice as many time as he wishes.Each result get added to his Round score.
+- But, if the player rolls a 1, all his Round score gets lost.After that, it's the next player's turn.
+- The player can choose to 'Hold', which means that his Round Score gets added to his GLOBAl score.After
+ that, it's the next players turn
+- The first player is to reach 100 points on GLOBAl score wins the game
+
+  */
+
+  
