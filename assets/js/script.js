aa63e3ff72b671096a1566d4ca225141f123070a
Moinul Hossain <moinulhossainmahim@gmail.com>
2021-02-04 16:11:47 +0600

    Added gamePlaying variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,34 +10,39 @@ GAME RULES:
 
   */
 (function() {
-	var scores, roundScore, activePlayer
+	var scores, roundScore, activePlayer, gamePlaying
 	init()
 
 	document.querySelector('.btn-roll').addEventListener('click',() => {
-		var dice = Math.floor(Math.random() * 6) + 1
-		var diceDom = document.querySelector('.dice')
-		diceDom.style.display = 'block'
-		diceDom.src=`./assets/img/dice-${dice}.png`
+		if(gamePlaying) {
+			var dice = Math.floor(Math.random() * 6) + 1
+			var diceDom = document.querySelector('.dice')
+			diceDom.style.display = 'block'
+			diceDom.src=`./assets/img/dice-${dice}.png`
 
-		if(dice !== 1) {
-			roundScore += dice
-			document.querySelector(`#current-${activePlayer}`).textContent = roundScore
-		} else {
-			nextPlayer()
+			if(dice !== 1) {
+				roundScore += dice
+				document.querySelector(`#current-${activePlayer}`).textContent = roundScore
+			} else {
+				nextPlayer()
+			}
 		}
 	})
 
 	document.querySelector('.btn-hold').addEventListener('click', () => {
-		scores[activePlayer] += roundScore
-		document.querySelector(`#score-${activePlayer}`).textContent = scores[activePlayer]
+		if(gamePlaying) {
+			scores[activePlayer] += roundScore
+			document.querySelector(`#score-${activePlayer}`).textContent = scores[activePlayer]
 
-		if(scores[activePlayer] >= 20) {
-			document.querySelector(`#name-${activePlayer}`).innerText = 'Winner!'
-			document.querySelector('.dice').style.display = 'none'
-			document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner')
-			document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active')
-		} else {
-			nextPlayer()
+			if(scores[activePlayer] >= 20) {
+				document.querySelector(`#name-${activePlayer}`).innerText = 'Winner!'
+				document.querySelector('.dice').style.display = 'none'
+				document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner')
+				document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active')
+				gamePlaying = false;
+			} else {
+				nextPlayer()
+			}
 		}
 	})
 
@@ -47,6 +52,7 @@ GAME RULES:
 		scores = [0, 0]
 		roundScore = 0
 		activePlayer = 0
+		gamePlaying = true
 		document.querySelector('.dice').style.display = 'none'
 
 		document.getElementById('score-0').textContent   = '0'
@@ -64,7 +70,7 @@ GAME RULES:
 
 	function nextPlayer() {
 		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
-			roundScore = 0
+		roundScore = 0
 		document.getElementById('current-0').textContent = '0'
 		document.getElementById('current-1').textContent = '0'
 
