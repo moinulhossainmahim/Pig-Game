cb189a154792064648ffba2e84f827e8bf2db1de
Moinul Hossain <moinulhossainmahim@gmail.com>
2021-02-04 15:53:07 +0600

    Added init and new button function

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,16 +11,7 @@ GAME RULES:
   */
 (function() {
 	var scores, roundScore, activePlayer
-	scores = [0,0]
-	roundScore = 0
-	activePlayer = 0
-
-	document.querySelector('.dice').style.display = 'none'
-
-	document.getElementById('score-0').textContent   = '0'
-	document.getElementById('score-1').textContent   = '0'
-	document.getElementById('current-0').textContent = '0'
-	document.getElementById('current-1').textContent = '0'
+	init()
 
 	document.querySelector('.btn-roll').addEventListener('click',() => {
 		var dice = Math.floor(Math.random() * 6) + 1
@@ -49,6 +40,27 @@ GAME RULES:
 			nextPlayer()
 		}
 	})
+
+	document.querySelector('.btn-new').addEventListener('click', init)
+
+	function init() {
+		scores = [0, 0]
+		roundScore = 0
+		activePlayer = 0
+		document.querySelector('.dice').style.display = 'none'
+
+		document.getElementById('score-0').textContent   = '0'
+		document.getElementById('score-1').textContent   = '0'
+		document.getElementById('current-0').textContent = '0'
+		document.getElementById('current-1').textContent = '0'
+		document.getElementById('name-0').textContent = 'Player 1'
+		document.getElementById('name-1').textContent = 'Player 2'
+		document.querySelector('.player-0-panel').classList.remove('winner')
+		document.querySelector('.player-1-panel').classList.remove('winner')
+		document.querySelector('.player-0-panel').classList.remove('active')
+		document.querySelector('.player-1-panel').classList.remove('active')
+		document.querySelector('.player-0-panel').classList.add('active')
+	}
 
 	function nextPlayer() {
 		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
